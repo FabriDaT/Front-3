fa1601a147cd807b66775bf85d944a9be12bf9fb
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2023-05-18 13:04:48 -0300

    commit desde pc escritorio

---
+++ clase-5-mesa-de-trabajo/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "./Card";
+
+
+
+function App() {
+
+  return (
+    <div style={{textAlign:'center'}}>
+
+      <h1>Produtos</h1>
+      <Card  style={{display:'flex',
+      flexDirection: 'column',
+      justifyContent:'center',
+      alignItems: 'center'}} />
+    </div>
+  )
+}
+
+export default App;
