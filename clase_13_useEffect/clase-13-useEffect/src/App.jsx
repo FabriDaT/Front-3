789b57b361ca806a734c083b346f2cff24a8c9a8
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2023-06-07 12:58:15 -0300

    commit

---
+++ clase_13_useEffect/clase-13-useEffect/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Pedido from './Pedido'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Pedido props={'pizza'}/>
+    </>
+  )
+}
+
+export default App
---
+++ clase_13_useEffect/clase-13-useEffect/src/Pedido.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+const Pedido = ({props}) => {
+useEffect( ()=>{ setTimeout 
+    } 
+)
+
+  return (
+    <div>
+
+      <h1> Pedido </h1>
+
+     {setTimeout((props) => {
+        <li>{props}</li>
+      }, 2000)}
+
+      <button> Cancelar pedido</button>
+
+    </div>
+  )
+}
+
+export default Pedido;
